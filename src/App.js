befe2ec6b5d7ec5fe53d9d79275c6675be74b324
yrk1999 <khasnisyash42@gmail.com>
2022-03-14 11:29:56 +0530

    First commit Message

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero'
+import Card from './components/Card';
+import data from './data.js';
+
+function App() {
+  const cards = data.map(item => {
+
+    return <Card
+      key={item.id}
+      item={item}
+    />
+  })
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <section className='cards-list'>
+        {cards}
+      </section>
+
+
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="/images/airbnb-logo.png" className="nav--logo" />
+        </nav>
+    )
+}
